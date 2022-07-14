f7740aa2929d8422cc3428e0a4bc1f768d445bbe
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2022-07-15 00:39:50 +0300

    :memo: UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,62 +1,36 @@
 
 #!/usr/bin/python3
-"""UTF-8 validation module.
-"""
+""" A method that checks if a given data set represents a valid UTF-8 """
+
+
+def EightBits(integer):
+    """ A function that:
+    converts an integer to its binary representation,
+    removes the 'Ob' prefixe
+    and returns the full 8 bits of the binary string """
+    binary = bin(integer)[2:]
+    add = '0' * (8 - len(binary))
+    return add + binary
 
 
 def validUTF8(data):
-    """Checks if a list of integers are valid UTF-8 codepoints.
-    See <https://datatracker.ietf.org/doc/html/rfc3629#page-4>
-    """
-    skip = 0
-    n = len(data)
-    for i in range(n):
-        if skip > 0:
-            skip -= 1
-            continue
-        if not isinstance(data[i], int) or data[i] < 0 or data[i] > 0x10ffff:
-            return False
-        elif data[i] <= 0x7f:
-            skip = 0
-        elif data[i] & 0b11111000 == 0b11110000:
-            # 4-byte utf-8 character encoding
-            span = 4
-            if n - i >= span:
-                next_body = list(map(
-                    lambda x: x & 0b11000000 == 0b10000000,
-                    data[i + 1: i + span],
-                ))
-                if not all(next_body):
-                    return False
-                skip = span - 1
-            else:
-                return False
-        elif data[i] & 0b11110000 == 0b11100000:
-            # 3-byte utf-8 character encoding
-            span = 3
-            if n - i >= span:
-                next_body = list(map(
-                    lambda x: x & 0b11000000 == 0b10000000,
-                    data[i + 1: i + span],
-                ))
-                if not all(next_body):
-                    return False
-                skip = span - 1
-            else:
-                return False
-        elif data[i] & 0b11100000 == 0b11000000:
-            # 2-byte utf-8 character encoding
-            span = 2
-            if n - i >= span:
-                next_body = list(map(
-                    lambda x: x & 0b11000000 == 0b10000000,
-                    data[i + 1: i + span],
-                ))
-                if not all(next_body):
-                    return False
-                skip = span - 1
-            else:
-                return False
+    """ A method that checks if a given data set represents a valid UTF-8 """
+    i = 0
+    while i < len(data):
+        if EightBits(data[i])[0] == '0':
+            i += 1
+        elif (EightBits(data[i])[:3] == '110' and i + 1 < len(data) and
+                EightBits(data[i + 1])[:2] == '10'):
+            i += 2
+        elif (EightBits(data[i])[:4] == '1110' and i + 2 < len(data) and
+                EightBits(data[i + 1])[:2] == '10' and
+                EightBits(data[i + 1])[:2] == '10'):
+            i += 3
+        elif (EightBits(data[i])[:5] == '11110' and i + 3 < len(data) and
+                EightBits(data[i + 1])[:2] == '10' and
+                EightBits(data[i + 2])[:2] == '10' and
+                EightBits(data[i + 3])[:2] == '10'):
+            i += 4
         else:
             return False
     return True
